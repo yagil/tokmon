2438c86be0b5aa1ba4aaa2bec43ab1328ff3fd3e
Yagil Burowski <yagil@elementlabs.ai>
2023-04-20 12:47:49 -0400

    bumpversion not working as expected

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(os.path.join(os.path.dirname(__file__), "README.md"), encoding="utf-8"
 
 setup(
     name='tokmon',
-    version='0.1.1',
+    version='0.1.3',
     packages=find_packages(),
     install_requires=[
         'mitmproxy',
